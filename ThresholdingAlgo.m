dba53f2dc9fd540e17be5ad8fe4c670578750d08
marie-mlnr <93117539+marie-mlnr@users.noreply.github.com>
2025-05-27 14:34:39 +0200

    Update ThresholdingAlgo.m

--- ThresholdingAlgo.m
+++ ThresholdingAlgo.m
@@ -1,3 +1,5 @@
+% This function is from: Brakel, J.P.G. van (2014). "Robust peak detection algorithm using z-scores". Stack Overflow. Available at: https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/22640362#22640362 (version: 2020-11-08).
+
 function [signals,avgFilter,stdFilter] = ThresholdingAlgo(y,lag,threshold,influence,T) %had, Cell also after ",influence"
 %Initialise signal results
 signals=zeros(length(y),1);
